1f20549a927e87032efcb7204128db6824f0aeda
kiswayODG <ulrichouedraogo500@gmail.com>
2024-06-14 21:22:02 +0000

    "initiate"

---
+++ src/test/java/com/ssr/id/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssr.id.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
